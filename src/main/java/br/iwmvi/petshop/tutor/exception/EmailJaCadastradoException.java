aa203fab3816f68d80d503e8901f4089b07b866a
Wallace Martins <wmvwallace@gmail.com>
2026-09-14 23:12:19 -0300

    refactor(exception): remover EmailJaCadastradoException do pacote tutor
    Remove o arquivo remanescente em br.iwmvi.petshop.tutor.exception. A classe foi movida para br.iwmvi.petshop.exception no refactor anterior; este commit elimina o caminho antigo que ficou retido no índice.

--- src/main/java/br/iwmvi/petshop/tutor/exception/EmailJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package br.iwmvi.petshop.tutor.exception;
-
-public class EmailJaCadastradoException extends RuntimeException {
-    public EmailJaCadastradoException(String email) {
-        super("Email ja cadastrado: " + email);
-    }
-}
